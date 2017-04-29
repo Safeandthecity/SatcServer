4d91ab7cbe45f42e1826b3ef55389b3564c4ba41
Jill Kowalchuk <jillkowalchuk@Jills-MacBook-Pro.local>
2017-04-29 19:31:31 +0100

    Added a getUser endpoint with hardcoded data

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var express = require('express')
+var jsonfile = require('jsonfile')
+
 var app = express()
 
+var data; 
+jsonfile.readFile("userSchema.json", function(err, obj) {
+    console.log(obj);
+    data = obj;
+})
+
 app.get('/', function (req, res) {
-  res.send('Hello Jill (or Jilian)!')
+  res.send("Hello Jill (or Jilian)");
+})
+
+app.get('/getUsers', function (req, res) {
+  res.json(data);
 })
 
 var port = process.env.PORT || 5000;
