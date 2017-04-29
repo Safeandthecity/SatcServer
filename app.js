b7ebbd0e2f906fed73543df414e4495ac9d2ec96
Jill Kowalchuk <jillkowalchuk@Jills-MacBook-Pro.local>
2017-04-29 19:51:22 +0100

    Adding endpoint for crime data (hardcoded)

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 var express = require('express')
 var jsonfile = require('jsonfile')
+var request = require('request')
 
 var app = express()
 
-var data; 
-jsonfile.readFile("userSchema.json", function(err, obj) {
+var data;
+jsonfile.readFile("userSchema.json", function (err, obj) {
     console.log(obj);
     data = obj;
 })
 
 app.get('/', function (req, res) {
-  res.send("Hello Jill (or Jilian)");
+    res.send("Hello Jill (or Jilian)");
 })
 
 app.get('/getUsers', function (req, res) {
-  res.json(data);
+    res.json(data);
+})
+
+app.get('/getCrimeData', function (req, res) {
+    request('https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2013-01', function (error, response, body) {
+        res.json(body);
+    });
 })
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+    console.log("Listening on " + port);
 });
 
